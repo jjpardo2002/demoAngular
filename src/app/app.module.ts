666caeb7e3b715eb67a0a6b7d8df9d9c6416d8ab
Johlver Pardo <jjpardo2002@gmail.com>
2020-08-24 15:23:46 -0500

    Inicio V2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './templates/menu/menu.component';
 import { QuienesComponent } from './components/quienes/quienes.component';
+import { FooterComponent } from './templates/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    QuienesComponent
+    QuienesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
