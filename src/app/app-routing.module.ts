1b9a971d928d606e3af9ab6c0356ce00e06e69d6
Johlver Pardo <jjpardo2002@gmail.com>
2020-08-24 15:21:07 -0500

    Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { QuienesComponent } from './components/quienes/quienes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: HomeComponent},
+  {path: 'miempresa', component: QuienesComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './templates/menu/menu.component';
+import { QuienesComponent } from './components/quienes/quienes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    QuienesComponent
   ],
   imports: [
     BrowserModule,
